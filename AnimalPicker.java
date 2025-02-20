79bef0b184a46981e6977cb0fe3a6425b1a88136
bjaxqq <brooksieee@proton.me>
2025-02-19 19:38:39 -0500

    Added the ability to have infinite inputs

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,15 +4,20 @@ public class AnimalPicker {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
 			System.out.println("Welcome to the Animal Picker!");
-			System.out.println("Which animal do you want to see, a dog or a cat?: ");
-			String animal = scanner.nextLine();
 
-			if (animal.equalsIgnoreCase("dog")) {
-			    System.out.println("DOG ASCII");
-			} else if (animal.equalsIgnoreCase("cat")) {
-			    System.out.println("CAT ASCII");
-			} else {
-			    System.out.println("Error: Invalid option! Please choose 'dog' or 'cat'.");
+			while (true) {
+			    System.out.println("Which animal do you want to see, a dog or a cat?: ");
+			    String animal = scanner.nextLine();
+
+			    if (animal.equalsIgnoreCase("dog")) {
+			        System.out.println("DOG ASCII");
+			        break;
+			    } else if (animal.equalsIgnoreCase("cat")) {
+			        System.out.println("CAT ASCII");
+			        break;
+			    } else {
+			        System.out.println("Error: Invalid option! Please choose 'dog' or 'cat'.");
+			    }
 			}
 		}
     }
